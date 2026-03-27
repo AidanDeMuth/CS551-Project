1cd9bf06286ec3159c0e8cf6e09a5a88e60fe7bc
Ubuntu <ubuntu@ip-172-31-21-130.us-east-2.compute.internal>
2026-03-27 14:16:11 +0000

    Install postgres fix

--- install/db_install.sh
+++ install/db_install.sh
@@ -13,7 +13,12 @@ if [ "$install_arg" = "install" ]; then
   sudo apt update
   sudo apt upgrade
 
-  sudo apt install postgresql
+  # Make a cluster
+  if ! pg_lsclusters | grep -q main; then
+    sudo pg_createcluster 16 main
+  fi
+
+  sudo apt install postgresql-16 postgresql-contrib
 elif [ "$install_arg" = "uninstall" ]; then
   # Delet config
   sudo apt purge postgresql
@@ -23,6 +28,7 @@ elif [ "$install_arg" = "uninstall" ]; then
   sudo deluser postgres
 
   # Get rid of all the little files
+  sudo apt purge -y postgresql-16 postgresql-contrib postgresql-common
   sudo apt autoremove -y
   sudo rm -rf /var/lib/postgresql/
   sudo rm -rf /etc/postgresql/
