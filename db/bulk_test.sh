6ca5312d2d43bf4906c7d942ba0e64481ed02eb3
Kiara Chau Garcia <kchaugarcia@student.ethz.ch>
2026-04-08 11:51:34 -0400

    Modify bulk_test.sh to only output the millis.

--- db/bulk_test.sh
+++ db/bulk_test.sh
@@ -4,7 +4,7 @@ set -e
 # I prob shouldn't be committing this but whatever
 CONN="postgresql://postgres:password@localhost:5432/testdb?connect_timeout=5"
 
-DROP="DROP TABLE IF EXISTS test_table;" > /dev/null
+DROP="DROP TABLE IF EXISTS test_table;"
 CREATE="
 	CREATE TABLE test_table(
 		id INT,
@@ -12,10 +12,10 @@ CREATE="
 	);
 "
 
-psql "$CONN" -c "$DROP" > /dev/null
-psql "$CONN" -c "$CREATE" > /dev/null
+psql "$CONN" -q -c "$DROP"
+psql "$CONN" -q -c "$CREATE"
 
 # Call some C function
 ./bulk_test
 
-psql "$CONN" -c "$DROP" > /dev/null
+psql "$CONN" -q -c "$DROP"
