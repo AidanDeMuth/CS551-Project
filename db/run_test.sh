629aec3fd3a577be7e73b05c40ce504c8b46a1e6
Kiara Chau Garcia <kchaugarcia@student.ethz.ch>
2026-04-08 12:00:40 -0400

    Refactor bulk_test.sh to customizable run_test.sh

--- db/run_test.sh
+++ db/run_test.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 set -e
 
+CPP_SCRIPT_NAME=$1
+if [[ $# -lt 1 ]]; then
+  echo "usage: [bulk_insert | mixed_rw]"; exit 1;
+fi
+
 # I prob shouldn't be committing this but whatever
 CONN="postgresql://postgres:password@localhost:5432/testdb?connect_timeout=5"
 
@@ -16,6 +21,6 @@ psql "$CONN" -q -c "$DROP" > /dev/null 2>/dev/null
 psql "$CONN" -q -c "$CREATE"
 
 # Call some C function
-./bulk_test
+./$CPP_SCRIPT_NAME
 
 psql "$CONN" -q -c "$DROP"
