635521a5dc940210aa69a5b0e735e401e42b1de4
Kiara Chau Garcia <kchaugarcia@student.ethz.ch>
2026-04-08 14:37:06 -0400

    Remove comments from bulk_insert.cpp

--- db/bulk_insert.cpp
+++ db/bulk_insert.cpp
@@ -7,22 +7,17 @@
 
 int main() {
     const int N = 10'000'000;
-	
+
 	Timer t;
 
 	pqxx::connection conn = getConnection("testdb");
 	pqxx::work tx{conn};
 
-    // Optional: truncate table before bulk insert
     tx.exec("TRUNCATE TABLE test_table;");
 
-    // Use stream_to for fast bulk insert
     pqxx::stream_to table_stream(tx, "test_table");
-
-    // Seed random generator
     std::srand(static_cast<unsigned int>(std::time(nullptr)));
 
-    
     for (long long i = 1; i <= N; ++i) {
         int id = i;
         int balance = std::rand() % 100000; // random balance
@@ -30,7 +25,6 @@ int main() {
         table_stream << std::make_tuple(id, balance);
     }
 
-    // Send all data to DB
     table_stream.complete();
 	tx.commit();
 
