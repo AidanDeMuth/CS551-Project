09cce1b78f2fcc67ca58ff13e9e8373d04c9ef1f
Kiara Chau Garcia <kchaugarcia@student.ethz.ch>
2026-04-08 14:56:48 -0400

    Add point queries per second test

---
+++ db/queries_per_second.cpp
@@ -0,0 +1,36 @@
+#include "./test.hh"
+#include "./timer.hh"
+
+#include <pqxx/pqxx>
+#include <iostream>
+#include <string>
+#include <random>
+
+const int NUM_QUERIES = 10'000;
+
+int main() {
+
+    pqxx::connection conn = getConnection("testdb");
+    pqxx::work W(conn);
+
+    pqxx::stream_to table_stream(tx, "test_table");
+    for (int i = 1; i <= NUM_QUERIES; ++i) {
+        table_stream << std::make_tuple(i, 30000);
+    }
+
+    Timer t;
+    std::unique_ptr<pqxx::work> W3 = std::make_unique<pqxx::work>(conn);
+    for (int i = 0; i < NUM_QUERIES; ++i) {
+
+        pqxx::result R = W3->exec("SELECT balance FROM test_table WHERE id=" + std::to_string(i) + ";");
+        if (i % 10000 == 0) {
+            W3->commit();
+            W3 = std::make_unique<pqxx::work>(conn);
+        }
+    }
+    W3->commit();
+
+    double duration_ms = t.getms();
+    std::cout << "Point query transactions per second: " << NUM_QUERIES*1000/duration_ms << ".\n";
+    return 0;
+}
